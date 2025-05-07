b8d9ab2117b326678189b453f1d4a8a078744a5f
dangat19 <daniel.muriithi@strathmore.edu>
2025-05-07 22:03:07 +0300

    Add files via upload

---
+++ Data Type challenge.java
@@ -0,0 +1,15 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int age = 20;
+        double grade = 89.5;
+        char gradeLetter = 'A';
+        boolean passed = true;
+        String name = "Daniel";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Grade Letter: " + gradeLetter);
+        System.out.println("Passed: " + passed);
+    }
+}
